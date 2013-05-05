ce94129424f08e7e6fcec599c0a4e363ece91045
Nitesh Patel <bitbucket@nitesh.name>
2013-05-05 23:15:06 +0100

    Store user settings -  Update game data using relevant user settings in start screen (Splash in our case) - Update and display current level and progress on game over screen

--- App.java
+++ App.java
@@ -1,15 +1,31 @@
 package com.herodevelop.hdlibgdx;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
 
 public class App {
     String tag;
+    Preferences prefs;
 
     public App(String tag) {
+        this.prefs = Gdx.app.getPreferences(tag);
         this.tag = tag;
     }
 
     public final void log(String s) {
         Gdx.app.log(this.tag, s);
+    }
+
+    public final String getPreference(String key, String defValue) {
+        return prefs.getString(key, defValue);
+    }
+
+    public final void setPreference(String key, String value) {
+        prefs.putString(key, value);
+    }
+
+    public final void flushPreferences() {
+        prefs.flush();
     }
 }
