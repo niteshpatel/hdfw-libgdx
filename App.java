3a36660a21033c64dcb712f499ec370ebbca74d0
Nitesh Patel <bitbucket@nitesh.name>
2013-05-05 13:37:17 +0100

    Pass in the tag used for logging, rather than hard-coding it

--- App.java
+++ App.java
@@ -3,8 +3,13 @@ package com.herodevelop.hdlibgdx;
 import com.badlogic.gdx.Gdx;
 
 public class App {
+    String tag;
+
+    public App(String tag) {
+        this.tag = tag;
+    }
 
     public final void log(String s) {
-        Gdx.app.log("hdfw", s);
+        Gdx.app.log(this.tag, s);
     }
 }
