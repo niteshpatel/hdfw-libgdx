6458c8c637c09fc8cef4f555404d9221d76800e1
Nitesh Patel <bitbucket@nitesh.name>
2013-05-11 21:53:29 +0100

    Add layer for playing sounds using libgdx

---
+++ Audio.java
@@ -0,0 +1,23 @@
+package com.herodevelop.hdlibgdx;
+
+import com.badlogic.gdx.Gdx;
+
+public class Audio {
+
+    public static class Sound {
+
+        com.badlogic.gdx.audio.Sound sound;
+
+        public Sound(String s) {
+            this.sound = Gdx.audio.newSound(Gdx.files.internal(s));
+        }
+
+        public void play(float volume) {
+            sound.play(volume);
+        }
+
+        public void dispose() {
+            sound.dispose();
+        }
+    }
+}
